8344aa0fea3e83c570b19a367d0768bfc3c357ff
Caue SJ <caue.silvaa@gmail.com>
2023-11-21 15:57:20 -0300

    Start create athlete table

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,26 @@
-import './style.css'
+document.addEventListener('DOMContentLoaded', () => {
+  getAthletesInfos()
+  getSwimmingStylesInfos()
+})
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Swimming Ranking</h1>
-    <h2>SESC Campinas</h2>
-    <p>
-      Simulado da Copa SESC Campinas - Natação
-    </p>
-  </div>
-`
+async function getAthletesInfos() {
+  const response = await fetch('http://localhost:3000/athletes')
+  const athletes = response.json();
+}
+
+async function getSwimmingStylesInfos() {
+  await fetch('http://localhost:3000/swimmingStyles') // URL from json-server end point
+  .then(data => data.json())
+  .then(result => {
+    createHeaderLine(result);
+  })
+}
+
+function createHeaderLine(swimmingStyles) {
+  const tableHeaderRow = document.querySelector('.table-header-row');
+  swimmingStyles.forEach(swimmingStyle => {
+    const th = document.createElement('th')
+    th.innerHTML = `${swimmingStyle.name} - ${swimmingStyle.distance}m`
+    tableHeaderRow.appendChild(th)
+  });
+}
